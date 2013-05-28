b20c3a03b086297023deec7d3ef2260a3c0a527a
Abdul <me@example.com>
2013-05-27 23:12:31 -0500

    drawing gameboard and cursor.  cursor moves. cursor is too thin

--- SwappidySwap-desktop/src/com/swappidy/swap/DesktopGame.java
+++ SwappidySwap-desktop/src/com/swappidy/swap/DesktopGame.java
@@ -3,6 +3,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class DesktopGame {
     public static void main (String[] args) {
-        new LwjglApplication(new Game(), "Game", 480, 320, false);
+        new LwjglApplication(new SwappidySwap(), "Game", 480, 320, false);
     }	
 }
