b3a4a8a04f7c29b6555f8bb5fa3829d7940b4b07
Abdul <me@example.com>
2013-05-27 19:02:47 -0500

    drawing some blocks

--- SwappidySwap/src/com/swappidy/swap/Game.java
+++ SwappidySwap/src/com/swappidy/swap/Game.java
@@ -1,24 +1,52 @@
 package com.swappidy.swap;
 
+import java.util.Random;
+
 import com.badlogic.gdx.ApplicationListener;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Game implements ApplicationListener {
 
+	public static final int BLOCK_SIZE = 3;
+	public static final int WORLD_HEIGHT = 10;
+	public static final int WORLD_WIDTH = 7;
+	
+	Color[] blockColors = new Color[]{
+			new Color(0, 1, 0, 1), // green
+			new Color(0, 0, 1, 1), // blue
+			new Color(1, 0, 0, 1), // red
+			new Color(1, 1, 0, 1), // purple
+	};
+	
 	SpriteBatch spriteBatch;
     BitmapFont font;
     CharSequence str = "Hello World!";
+    int screenWidth, screenHeight;
+    private OrthographicCamera cam;
+    Random rng = new Random();
+
+	ShapeRenderer debugRenderer;
     
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-
-        
         spriteBatch = new SpriteBatch();
         font = new BitmapFont();
-
-        
+        new Pixmap(32, 32, Pixmap.Format.RGB565);
+        screenWidth = Gdx.graphics.getWidth();
+		screenHeight = Gdx.graphics.getHeight();
+        debugRenderer = new ShapeRenderer();
+        this.cam = new OrthographicCamera(WORLD_HEIGHT, WORLD_WIDTH);
+		this.cam.position.set(5, 3.5f, 0);
+		this.cam.update();
 	}
 
 	@Override
@@ -29,10 +57,19 @@ public class Game implements ApplicationListener {
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
-		spriteBatch.begin();
-        font.draw(spriteBatch, str, 30, 30);
-        spriteBatch.end();
+		GL10 gl = Gdx.graphics.getGL10();
+		gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		gl.glClearColor(1, 1, 1, 0);
+
+		debugRenderer.setProjectionMatrix(cam.combined);
+		debugRenderer.begin(ShapeType.Filled);
+		for(int i = 0; i < WORLD_WIDTH; i++){
+			for(int j = 0; j < WORLD_HEIGHT; j++){
+				debugRenderer.setColor(blockColors[rng.nextInt(blockColors.length)]);
+				debugRenderer.rect(i, j, 1,1);
+			}
+		}
+		debugRenderer.end();
 	}
 
 	@Override
